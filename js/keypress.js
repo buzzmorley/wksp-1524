c25814221c0b177a516bf540bafa34af83def710
buzzmorley <buzz@blu.bike>
2017-04-29 21:24:25 +0000

    keypress.html keypress.js

---
+++ js/keypress.js
@@ -0,0 +1,17 @@
+var el;                                              // Declare variables
+
+function charCount(e)   {                           // Declare function
+  var textEntered, charDisplay, counter, lastkey;   // Declare variables
+  textEntered = document.getElementById('message').value; // Users text
+  charDisplay = document.getElementById('charactersLeft'); // Counter element
+  counter = (180 - (textEntered.length));           // Number of chars left
+  charDisplay.textContent = counter;                // Show charcters left
+  
+  
+  lastkey = document.getElementById('lastkey');     // Get last key element
+  lastkey.textContent = 'Last key by ASCII code: ' + e.keyCode; // Create msg
+  
+} 
+
+el = document.getElementById('message');            // Get msg element
+el.addEventListener('keyup', charCount, false);     // keyup event
