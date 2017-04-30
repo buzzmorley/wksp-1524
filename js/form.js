089716acd2589e220dfd33598213c0af1cb0bd70
buzzmorley <buzz@blu.bike>
2017-04-30 19:38:16 +0000

    form.js

---
+++ js/form.js
@@ -0,0 +1,28 @@
+/*eslint-env browser */
+var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint;     
+elForm          = document.getElementById('formSignup');         // Store elements  
+elSelectPackage = document.getElementById('package');
+elPackageHint   = document.getElementById('packageHint');
+elTerms         = document.getElementById('terms');
+elTermsHint     = document.getElementById('termsHint');
+
+function packageHint()  {                                           // Declare function
+    var pack = this.options[this.selectedIndex].value;              // Get selected option
+    if (pack ==='monthly')  {                                       // If monthly package 
+    elPackageHint.innerHTML = 'Save $10 if you pay for one year';   // Show this message
+    } else {                                                        // Otherwise
+      elPackageHint.innerHTML = 'Wise choice!';                     // Show this message
+    }
+} 
+
+function checkTerms(event)  {                                       // Declare function
+  if (!elTerms.checked)     {                                       // If checkbox not ticked
+    elTermsHint.innerHTML = ' You must agree to the terms.';        // Show message
+    event.preventDefault();                                         // Don't submit form
+  }
+}
+
+// Create event listeners: submit calls checkTerms(), change calls packageHint()
+elForm.addEventListener('submit', checkTerms, false);
+elSelectPackage.addEventListener('change', packageHint, false);
+
